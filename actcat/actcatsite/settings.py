3025c32c55b2ceaabf16633464ffe174e70adfa5
Colleen Dunlap <colleendunlap@Colleens-Air.lan>
2025-03-27 00:14:28 -0400

    using database, remove db from repo

--- actcat/actcatsite/settings.py
+++ actcat/actcatsite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'actcatalog.apps.ActcatalogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
